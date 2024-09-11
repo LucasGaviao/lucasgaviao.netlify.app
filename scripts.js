e9e84ebb2fb5c56e8e58bb291858b20e6bdbebbd
Lucas Gaviao <143828910+LucasGaviao@users.noreply.github.com>
2024-09-11 02:51:44 -0300

    Atualização de Projeto
    Atualizados: Index.html & styles.css
    Criado: pagina2.html
    Renomeado e Atualizado: teste3.js -> scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function alerta(){
+    alert("Esta funcionalidade está incompleta desculpe pelo transtorno.");
+}
+
+function perguntar(){
+    var nome;
+    nome = prompt("Qual é o seu nome? ");
+    alert("Olá " + nome);
+}

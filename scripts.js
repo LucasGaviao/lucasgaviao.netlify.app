758bd860363aa16c9bdf04d78221ab7bfd84433a
Lucas Gaviao <143828910+LucasGaviao@users.noreply.github.com>
2024-09-12 03:33:23 -0300

    Update de temas
    Adições;
    possibilidade de alterar o tema da pagina.
    Atualizações:
    Nova box-shadow para os itens em foco. Animação no botão Enviar.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,24 @@
+const $html = document.querySelector('html')
+const $theme_button = document.querySelector('#theme-button')
+
+function change_theme(){
+    $html.classList.toggle('light-mode')
+
+    // Alterna o texto do botão
+    if ($theme_button.textContent === 'Light') {
+        $theme_button.textContent = 'Dark'
+    } else {
+        $theme_button.textContent = 'Light'
+    }
+}
+
+$theme_button.addEventListener('click', change_theme)
+
 function alerta(){
     alert("Esta funcionalidade está incompleta desculpe pelo transtorno.");
 }
 
+// nao esta sendo usado
 function perguntar(){
     var nome;
     nome = prompt("Qual é o seu nome? ");

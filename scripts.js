4383864da5b8661eb2d9670bfaf80d5db791113f
Lucas Gaviao <143828910+LucasGaviao@users.noreply.github.com>
2024-09-14 18:05:39 -0300

    Created Forms

--- scripts.js
+++ scripts.js
@@ -18,65 +18,68 @@ function alerta(){
     alert("Esta funcionalidade está incompleta desculpe pelo transtorno.");
 }
 
-class FormSubmit{
-    constructor(settings){
-        this.settings = settings;
-        this.form = document.querySelector(settings.form);
-        this.formButton = document.querySelector(settings.button);
-        if (this.form){
-            this.url = this.form.getAttribute("action");
-        }     
-        this.sendForm = this.sendForm.bind(this);
+class FormSubmit {
+    constructor(settings) {
+      this.settings = settings;
+      this.form = document.querySelector(settings.form);
+      this.formButton = document.querySelector(settings.button);
+      if (this.form) {
+        this.url = this.form.getAttribute("action");
+      }
+      this.sendForm = this.sendForm.bind(this);
     }
-
+  
     displaySuccess() {
-        this.form.innerHTML = this.settings.success;
+      this.form.innerHTML = this.settings.success;
     }
+  
     displayError() {
-        this.form.innerHTML = this.settings.error;
+      this.form.innerHTML = this.settings.error;
     }
-
+  
     getFormObject() {
-        const formObject = {};
-        const fields = this.fields.querySelector("[name]");
-        fields.forEach((field) => {
-            formObject[field.getAttribute("name")] = field.value;
-        });
-        return formObject;
+      const formObject = {};
+      const fields = this.form.querySelectorAll("[name]");
+      fields.forEach((field) => {
+        formObject[field.getAttribute("name")] = field.value;
+      });
+      return formObject;
     }
-
-    onSubmission(event){
-        event.preventDefault();
-        event.target.disabled = true;
-        event.target.innerText = "Enviando...";
+  
+    onSubmission(event) {
+      event.preventDefault();
+      event.target.disabled = true;
+      event.target.innerText = "Enviando...";
     }
-
-    async sendForm(event){
-        try{
-            this.onSubmission(event);
-            await fetch(this.url, {
-                method:"POST",
-                headers:{
-                    "Content-Type": "application/json",
-                    Accept: "application/json"
-                },
-                body: JSON.stringify(this.getFormObject()),
+  
+    async sendForm(event) {
+      try {
+        this.onSubmission(event);
+        await fetch(this.url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify(this.getFormObject()),
         });
         this.displaySuccess();
-        }catch(error){
-            throw new Error(error);
-
-        }
+      } catch (error) {
+        this.displayError();
+        throw new Error(error);
+      }
     }
-    init(){
-        if (this.form) this.formButton.addEventListener("click", this.sendForm);
-            return this;
+  
+    init() {
+      if (this.form) this.formButton.addEventListener("click", this.sendForm);
+      return this;
     }
-}
-
-const formSubmit = new FormSubmit({
+  }
+  
+  const formSubmit = new FormSubmit({
     form: "[data-form]",
     button: "[data-button]",
     success: "<h1 class='success'>Mensagem enviada!</h1>",
-    error: "<h1 class='error'>Não foi possivel enviar sua mensagem.</h1>",
-});
+    error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
+  });
+  formSubmit.init();
